2e02df862ccd725f318f09731cbaae514c45eb9e
Dacre <enquiries@mooce.co.nz>
2018-07-01 12:17:19 +1200

    documentation

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
-const { execSync } = require('child_process');
+var commandExistsSync = require('command-exists').sync;
+const {
+    execSync
+} = require('child_process');
 
 const OUTPUT_PREFIX = 'processed_'
 const OUTPUT_NAME = 'output'
 const TIME_PER_IMAGE = '4'
 
-function deleteFiles(filepath) {
+/**
+ * Logs the precentage of progress for an active process
+ * @param {*} idx 
+ * @param {*} list 
+ */
+function logProgress(idx, list) {
 
-    const filename = path.basename(filepath);
+    if (list.length) {
 
-    if (filename.match(`${OUTPUT_PREFIX}*`, 'gi')) {
-        console.log('Delete')
-        fs.unlinkSync(filepath);
-        return false;
-    } else {
-
-        return true
+        console.info(`${ parseInt((idx * 100) / list.length) }%`);
     }
 }
 
-function readFiles(filepath) {
-    
-    return fs.readdirSync(filepath)
-    .map(file => path.join(filepath, file))
+/**
+ * Deletes a file if file's path matches processed prefix
+ * @param {*} filepath 
+ */
+function deletePrefixedFile(filepath, idx, list) {
+
+    logProgress(idx, list);
+
+    const filename = path.basename(filepath) || '';
+
+    if (filename.match(`${OUTPUT_PREFIX}_[a-zA-Z-_]+.jpg$`, 'gi')) {
+
+        console.info(`deleting file ${ filepath }`);
+        fs.unlinkSync(filepath);
+    }
 }
 
-function processFile(file, index, list) {
+/**
+ * Processes input image file. Applies uniform/box resize, centers and captions the image. The result is prefixed with OUTPUT_PREFIX for subsequent processing
+ * @param {*} file 
+ * @param {*} idx 
+ * @param {*} list 
+ */
+function processFile(file, idx, list) {
 
-    console.log(`${ parseInt((index * 100) / list.length) }%`);
+    logProgress(idx, list);
 
     var matches = file.match(/[A-Z\-]+.jpg/gi);
 
@@ -43,21 +63,43 @@ function processFile(file, index, list) {
         .replace(/(?:^|\s)\S/g, c => c.toUpperCase())
         .replace(/and/gi, 'and')
         .replace(/with/gi, 'with');
-
-    console.log(caption)
     console.log('file', file)
+    console.log(caption)
+    
+    return execSync(`magick "${file}" -gravity Center -background black -resize 1920x1080 -extent 1920x1080 -font Arial-Bold -pointsize 60 -stroke black -strokewidth 2 -fill white -gravity SouthEast -draw "text 30,30 '${ caption }'" ${ OUTPUT_PREFIX }${ idx }.jpg`)
+}
 
-    const cmd = `magick ${file} -gravity Center -background black -resize 1920x1080 -extent 1920x1080 -font Arial-Bold -pointsize 60 -stroke black -strokewidth 2 -fill white -gravity SouthEast -draw "text 30,30 '${ caption }'" ${ OUTPUT_PREFIX }${ index }.jpg`
+function sequenceImages() {
 
-    return execSync(cmd)
+    execSync(`ffmpeg -r 1/${ TIME_PER_IMAGE } -i ${ OUTPUT_PREFIX }%d.jpg -c:v libx264 -vf "fps=25,format=yuv420p" ${ OUTPUT_NAME }.mp4`, {
+        stdio: [0, 1, 2]
+    })
+}
+
+function readFiles(filepath) {
+
+    return fs.readdirSync(filepath)
+        .map(file => path.join(filepath, file))
+}
+
+
+if(!commandExistsSync('ffmpeg')) {
+    throw new Error('ffmpeg is not installed. install ffmpeg version 3.4. https://ffmpeg.org/download.html')
+}
+
+if(!commandExistsSync('magick')) {
+    throw new Error('magick not found. install ImageMagick 7.0.8. https://www.imagemagick.org/script/index.php')
 }
 
 const files = readFiles('E:\/test\/')
 
-files.forEach(deleteFiles)
+files.forEach(deletePrefixedFile);
 
-console.log(files)
+files.forEach(processFile); 
+
+sequenceImages()
 /*
+console.log(files)
 
 
 const files = readFiles('E:\/test')
