59e31f006f809d7d68db8703ed7e823ab0f59957
Dacre <enquiries@mooce.co.nz>
2018-07-14 22:33:36 +1200

    added addition switches for process control, can optionally control if image/video processing occurings

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ supported argument:
     -i : specify source directory for photos
     -o : specify destination directory for output_video.mp4
     -d : specify the duration in seconds for each photo in the output video
+    -process-images [true|false] : controls if photos in source direction are processed (defaults to true)
+    -process-video [true|false] : controls video is generated from processed photos (defaults to true)
     -h : display this help summary
     `)
 }
@@ -34,7 +36,18 @@ else {
         throw Error('duration must be 1 or more')
     }
     
-    Process.deleteProcessedFile();
-    Process.processImages(inputPath);
-    Process.sequenceImages(outputPath, duration);
+    if(Args.getArg('-process-images', 'true') !== 'false') {
+        Process.deleteProcessedFile();
+        Process.processImages(inputPath);
+    }
+    else {
+        console.log('Skipping image processing')
+    }
+    
+    if(Args.getArg('-process-video', 'true') !== 'false') {
+        Process.sequenceImages(outputPath, duration);
+    }
+    else {
+        console.log('Skipping video processing')
+    }
 }
